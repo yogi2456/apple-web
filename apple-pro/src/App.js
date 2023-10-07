5c8ad2764eb10567d35dc6ebfd31a779094d357b
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-08 00:59:22 +0530

    updates

---
+++ apple-pro/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Components/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ apple-pro/src/Components/Homepage.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <div>
+      <div className='navbar'>
+        <div className='navbar1'>
+            <p><i class="fa-brands fa-apple fa-xl" style={{color: "#FFFFFF"}}></i></p>
+            <p>Store</p>
+            <p>Mac</p>
+            <p>iPad</p>
+            <p>iPhone</p>
+            <p>Watch</p>
+            <p>Vision</p>
+            <p>AirPods</p>
+            <p>Tv & Home</p>
+            <p>Entertainment</p>
+            <p>Accessories</p>
+            <p>Support</p>
+            <p><i class="fa-solid fa-magnifying-glass fa-xl" style={{color: "rgb(173, 169, 169)"}}></i></p>
+            <p><i class="fa-solid fa-bag-shopping fa-xl" style={{color: "rgb(173, 169, 169)"}}></i></p>
+        </div>
+
+        <div className='black'>
+        <h1>iPhone 15 Pro</h1>
+            <h3>Titanium. So strong. So light. So Pro</h3>
+            <div className='black1'>
+                <div className='black2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='black3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+
+        <div className='iphoneimg'></div>
+        <div className='white'>
+        <h1>iPhone 15 Pro</h1>
+            <h3>New camera. New design. Newphoria</h3>
+            <div className='white1'>
+                <div className='white2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='white3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+        <div className='iphoneimg1'></div>
+
+        <div className='watch'>
+        <h1>iPhone 15 Pro</h1>
+            <h3>New camera. New design. Newphoria</h3>
+            <div className='watch1'>
+                <div className='watch2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='watch3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+        <div className='iphoneimg2'></div>
+
+        <div className='image'>
+            <div className='image1'>
+                <div className='image2'>
+                <div className='watchi'>
+            <h1>iPhone 15 Pro</h1>
+            <h3>New camera. New design. Newphoria</h3>
+            <div className='watchi1'>
+                <div className='watchi2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='watchi3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+                </div>
+                <div className='image3'>
+                    <div className='girl'>
+                        <p><i class="fa-brands fa-apple fa-xl" style={{color: "#FFFFFF"}}></i></p>
+                        <h1>Vision Pro</h1>
+                    </div>
+                    <h2>Welcome to the era of spatial Computing</h2>
+                    <p>Available next year in the U.S.</p>
+                    <div className='girl1'>
+                        <p>Learn more</p>
+                        <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div className='image'>
+            <div className='image1'>
+                <div className='clock2'>
+                <div className='watchii'>
+            <h1>MacBook Air 15" </h1>
+            <h3>Impressively big. Impossibly thin.</h3>
+            <div className='watchi1'>
+                <div className='watchi2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='watchi3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+                </div>
+                <div className='clock3'>
+                <div className='watchii'>
+            <h1>AirPods Pro</h1>
+            <h3>Adaptive Audio. Now playing</h3>
+            <div className='watchi1'>
+                <div className='watchi2'>
+                    <p>Learn more</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+                <div className='watchi3'>
+                    <p>Buy</p>
+                    <p><i class="fa-solid fa-chevron-right fa-l" style={{color: "#145fe1"}}></i></p>
+                </div>
+            </div>
+        </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='full'>
+            <div className='full1'>
+            <img style={{width: "100%", height: "100%"}} src='https://is1-ssl.mzstatic.com/image/thumb/-AiwUH5SvHJdsktfhM7pAw/1250x703.jpg'/>
+            </div>
+        </div>
+
+        <div className='footer'>
+            <div className='footer1'>
+                <p className='footer2'>
+                1. Trade-in values will vary based on the condition, year, and configuration of your eligible trade-in device. Not all devices are eligible for credit. You must be at least 18 years old to be eligible to trade in for credit or for an Apple Gift Card. Trade-in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores, and may vary between in-store and online trade-in. Some stores may have additional requirements. Apple or its trade-in partners reserve the right to refuse or limit quantity of any trade-in transaction for any reason. More details are available from Apple’s trade-in partner for trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+                </p>
+                <p>
+Apple Vision Pro has not been authorized as required by the rules of the Federal Communications Commission. This device is not, and may not be, offered for sale or lease, or sold or leased, until authorization is obtained</p>
+                <p>Apple Vision Pro will be available early next year on apple.com and at Apple retail stores in the U.S.</p>
+                <p>To access and use all Apple Card features and products available only to Apple Card users, you must add Apple Card to Wallet on an iPhone or iPad that supports and has the latest version of iOS or iPadOS. Apple Card is subject to credit approval, available only for qualifying applicants in the United States, and issued by Goldman Sachs Bank USA, Salt Lake City Branch.</p>
+                <p>If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about Apple Card.</p>
+            </div>
+        </div>
+      </div>
+    </div>  
+
+      
+    
+  )
+}
+
+export default Homepage
