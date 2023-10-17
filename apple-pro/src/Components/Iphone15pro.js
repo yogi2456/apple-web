a65c3856a9c57af00e480a07c8e3e79802d12754
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-17 16:29:13 +0530

    updates

--- apple-pro/src/Components/Iphone15pro.js
+++ apple-pro/src/Components/Iphone15pro.js
@@ -63,10 +63,195 @@ function Iphone15pro() {
                         <p></p>
                     </button>
                     <div>
-                        <p></p>
+                        <p><i class="fa-solid fa-circle-play" style={{color: "#9b9b9d"}}></i></p>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div className='ipro10'>
+            <div className='ipro11'>
+                <h1>Take a closer look.</h1>
+                <img style={{width:"45%", height: "50%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/closer-look/all_colors__eppfcocn9mky_large.jpg'/>
+                <p>6.1” iPhone 15 Pro2 in four colors</p>
+                <button>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </button>
+            </div>
+        </div>
+
+
+        <div className='ipro12'>
+            <div className='ipro13'>
+                <h2>Explore the full story</h2>
+                <div className='ipro14'>
+                    <h1>iPhone.</h1>
+                    <h1>Forged in titanium.</h1>
+                </div>
+                <div className='ipro15'>
+                    <div>
+                        <p>
+                            <img style={{width: "100%", height: "100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/design/titanium_endframe__jkbvub6cqj6u_large.jpg'/>
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            <img style={{width: "100%", height: "100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/design/lightest__boybgwifuehe_large.jpg'/>
+                        </p>
+                        <p>
+                            <img style={{width: "100%", height: "100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/design/durable__omxi3w443rue_large.jpg'/>
+                        </p>
+                    </div>
+                </div>
+
+                <div className='ipro16'>
+                    <p>iPhone 15 Pro is the first iPhone to feature an aerospace‑grade titanium design, using the same alloy that spacecraft use for missions to Mars.</p>
+                    <p>Titanium has one of the best strength‑to‑weight ratios of any metal, making these our lightest Pro models ever. You’ll notice the difference the moment you pick one up.</p>
+                </div>
+
+                <button>
+                    <p>More on design & display</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                </button>
+            </div>
+        </div>
+
+
+        <div className='ipro17'>
+            <div className='ipro18'>
+                <img style={{width: "15%", height: "10%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/chip/chip__fuct924q1s66_large.jpg'/>
+                <div className='ipro19'>
+                    <p>A17 Pro chip.</p>
+                    <p>A monster win for gaming.</p>
+                    <p>It’s here. The biggest redesign in the history of Apple GPUs.</p>
+                </div>
+                <div className='ipro20'>
+                    <p>
+                        <img style={{width: "95%", height: "90%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/chip/performance_endframe__ee48m49gny0y_large.jpg'/>
+                    </p>
+                </div>
+                <div className='ipro21'>
+                    <div>
+                    <p>A17 Pro is an entirely new class of iPhone chip that delivers our best graphics performance by far.Mobile games will look and feel so immersive, with incredibly detailed environments and more realistic characters. And with industry-leading speed and efficiency, A17 Pro takes fast and runs with it.</p>
+                    </div>
+                    <div>
+                        <p>New</p>
+                        <p>Pro-class GPU</p>
+                        <p>with 6 cores</p>
+                    </div>
+                </div>
+                <button>
+                    <p>Go deeper on A17 Pro</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                </button>
+            </div>
+        </div>
+
+
+        <div className='ipro22'>
+            <div className='ipro23'>
+                <h1>A camera that captures your wildest imagination.</h1>
+                <h3>From dramatic framing flexibility to next-generation portraits, see what you can do with our most powerful iPhone camera system.</h3>
+                <p>A detailed photo of a green iguana, captured with the 48MP Main camera on iPhone 15 Pro camera</p>
+            </div>
+            <div className='ipro24'>
+                <p>
+                    <img style={{width:"100%", height:"100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/camera/camera__bo5k5tfk6cmu_large.jpg'/>
+                </p>
+            </div>
+            <div className='ipro25'>
+                <p>A detailed photo of a green iguana, captured with the 48MP Main camera on iPhone 15 Pro camera. A green iguana, captured by the 48MP Main camera</p>
+                <p>With iPhone 15 Pro, you have multiple focal lengths to work with. It’s like having seven pro lenses in your pocket, everywhere you go.</p>
+                <p>
+                    <img style={{width: "100%", height: "100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/camera/pro_lens1__brxt4ho5qkeu_large.jpg' />
+                </p>
+                <p>0.5x Ultra Wide | Macro</p>
+                <div className='ipro26'>
+                    <div>
+                        <p>
+                            <img style={{width: "100%", height:"100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/camera/48mp_camera__cxq25ak34nau_large.jpg'/>
+                        </p>
+                    </div>
+                    <div>
+                        <p>The 48MP Main camera is more advanced than ever, capturing super‑high‑resolution photos with a new level of detail and color.</p>
+                        <p>You’ll see the improvements in your portraits. And now you no longer have to switch to Portrait mode. If your subject is a person, dog, or cat, iPhone automatically captures depth information. So you can choose to instantly see your photo as a portrait, with an artful blur effect. Or later in the Photos app.</p>
+                    </div>
+                </div>
+                <p className='ipro27'></p>
+                <button>Spatial video capture available later this year</button>
+                <h1>Shoot magical spatial videos,<br />then relive them on Apple Vision Pro</h1>
+                <div className='ipro28'>
+                <button>
+                    <p>Go deeper on A17 Pro</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                </button>
+                </div>
+            </div>
+        </div>
+
+        <div className='ipro29'>
+                    <div className='ipro30'>
+                    <p>120 mm of <br />pure Pro zoom.</p>
+                    <p>
+                        <img style={{width: "100%", height:"100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/5x-zoom/pro-zoom_endframe__bpmc72f8qwgi_large.jpg'/>
+                    </p>
+                    <div className='ipro31'>
+                        <div>
+                            <p>For iPhone 15 Pro Max, we designed a 5x Telephoto camera with the longest optical zoom of any iPhone ever to fit in our compact Pro camera system.</p>
+                            <p>Now you can take sharper close‑ups from farther away — like a phenomenal photo of your friend or a goal in your kid’s soccer match.</p>
+                        </div>
+                        <div>
+                            <p>5x optical<br /> zoom</p>
+                            <p>with the 120 mm lens</p>
+                        </div>
+                    </div>
+                    <button>
+                    <p>Nerd out on 5x Telephoto</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                    </button>
+                    </div>
+        </div>
+
+
+        <div className='ipro32'>
+                    <div className='ipro33'>
+                    <p>Gigablast <br /> your gigabits.</p>
+                    <p>
+                        <img style={{width: "100%", height:"100%"}} src='https://www.apple.com/v/iphone-15-pro/b/images/overview/usb-c/usbc__dqdn0phay4mu_large.jpg'/>
+                    </p>
+                    <div className='ipro31'>
+                        <div>
+                            <p>iPhone 15 Pro is the first iPhone to support USB 3,6 for a huge leap in data transfer speeds and faster pro workflows than ever before.</p>
+                            <p>The new USB‑C connector lets you charge your Mac or iPad with the same cable you use to charge iPhone 15 Pro. Bye‑bye, cable clutter.</p>
+                        </div>
+                        <div>
+                            <p>Up to <br /> 20x faster <br /> file transfers</p>
+                            <p>And with all‑new Wi‑Fi 6E7 you'll get two times faster wireless speeds. So you can upload, download, and transfer files in a flash.</p>
+                        </div>
+                    </div>
+                    <button>
+                    <p>Explore Connectivity</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                    </button>
+
+                    <div className='ipro34'>
+                        <h1>In an emergency, <br />iPhone has your back.</h1>
+                    </div>
+                    <div className='ipro35'>
+                        <p>New Roadside Assistance via satellite14 can get you help for things like a flat tire or a dead battery, even when you’re off the grid.</p>
+                        <p>iPhone also has Emergency SOS via satellite14 and Crash Detection15, two vital safety features that have helped save lives.</p>
+                    </div>
+                    <div className='ipro36'>
+                    <button>
+                    <p>Nerd out on 5x Telephoto</p>
+                    <p><i class="fa-solid fa-circle-plus" style={{color: "#1461e6"}}></i></p>
+                    </button>
+                    </div>
+                    </div>
         </div>
     </>
   )
