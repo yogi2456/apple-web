e4de509b9c0da49d59f74feb3bfe4d6fdf05425a
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-25 12:06:47 +0530

    updates

--- apple-pro/src/Components/Shophone1.js
+++ apple-pro/src/Components/Shophone1.js
@@ -3,7 +3,7 @@ import './Shophone1.css'
 
 function Shophone1() {
   return (
-    <div>
+    <>
       <div className='navbar'>
         <div className='navbar1'>
             <p><i class="fa-brands fa-apple fa-xl" style={{color: "rgb(173, 169, 169)"}}></i></p>
@@ -56,8 +56,288 @@ function Shophone1() {
             </div>
           </div>
         </div>
-        </div>
+
+        <div className='shop7'>
+            <p>All Models</p>
+            <p>Shopping Guides</p>
+            <p>Ways to Save</p>
+            <p>Setup and Support</p>
+            <p>Accessories</p>
+            <p>The iPhone Experience</p>
+            <p><i class="fa-solid fa-angle-right" style={{color: "#bebfc1"}}></i></p>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>All models.</h1>
+              <h1>Take your pic.</h1>
+            </div>
+            <div className='shop10'>
+              <div className='shop11'>
+                <p>New</p>
+                <h1>iPhone 15 Pro & <br/> iPhone 15 Pro Max</h1>
+                <img style={{width: "80%", height: "50%"}} src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290312'/>
+                <div className='shop12'>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+                <div className='shop13'>
+                  <p>From $999 or $41.62/mo.for 24 mo.*</p>
+                  <button>Buy</button>
+                </div>
+              </div>
+              <div className='shop11'>
+                <p>New</p>
+                <h1>iPhone 15 & <br/> iPhone 15 Plus</h1>
+                <img style={{width: "80%", height: "50%"}} src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone15hero-202309?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290559'/>
+                <div className='shop12'>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+                <div className='shop13'>
+                  <p>From $799 or $33.29/mo.for 24 mo.*</p>
+                  <button>Buy</button>
+                </div>
+              </div>
+              <div className='shop11'>
+                <p>New</p>
+                <h1>iPhone 14 & <br/> iPhone 14 Plus</h1>
+                <img style={{width: "80%", height: "50%"}} src='https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14-202209?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290654'/>
+                <div className='shop12'>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                  <p></p>
+                </div>
+                <div className='shop13'>
+                  <p>From $699 or $29.12/mo.for 24 mo.*</p>
+                  <button>Buy</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>Shopping guides.</h1>
+              <h1>Can’t decide? Start here.</h1>
+            </div>
+            <div className='shop14'>
+              <div className='shop15'>
+                <p>COMPARE ALL MODELS</p>
+                <h1>Which iPhone is <br/> right for you?</h1>
+              </div>
+              <div className='shop16'>
+                <p>NEW</p>
+                <h1>Shop with a specialist <br/> over video.</h1>
+                <h2>Choose your next iphone <br/> in a guided, one-way <br/> video session</h2>
+              </div>
+              <div className='shop17'>
+                <p>WATCH AND LEARN</p>
+                <h1>How to trade in your <br/>iPhone.</h1>
+                <h2>Get a quick one-minute overview from Bella.</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>Ways to save.</h1>
+              <h1>Find what works for you.</h1>
+            </div>
+            <div className='shop14'>
+              <div className='shop18'>
+                <p>APPLE TRADE IN</p>
+                <h1>Get $200–$650 in credit <br/> toward iPhone 15 or <br/> iPhone 15 Pro when you <br/> trade in an iPhone 11 or<br/> higher.2</h1>
+              </div>
+              <div className='shop19'>
+                <p>CARRIER TRADE-IN DEALS</p>
+                <h1>iPhone carrier deals.<br/> Right here at Apple.<br/> For as low as $0.¹</h1>
+                <h2>Includes trade-in and carrier credits.<br/> AT&T: up to $1000. T-Mobile: up to $800.<br/> Verizon: up to $830.</h2>
+              </div>
+              <div className='shop20'>
+                <p>APPLE CARD</p>
+                <h1>Pay 0% APR over 24<br/> months when you choose to<br/> check out with Apple Card <br/> Monthly Installments.3</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>Setup and support.</h1>
+              <h1>Our Specialists are here to help.</h1>
+            </div>
+            <div className='shop14'>
+              <div className='shop21'>
+                <p>WATCH AND LEARN</p>
+                <h1>Use Quick Start to set up <br/> and transfer data.</h1>
+                <h2>Watch Jeannie’s one‑minute overview <br/> on how easy it can be.</h2>
+              </div>
+              <div className='shop22'>
+                <p>SETUP GUIDE</p>
+                <h1>Step-by-step on how to <br/> easily setup your new <br/> iPhone.</h1>
+              </div>
+              <div className='shop23'>
+                <p>WATCH AND LEARN</p>
+                <h1>Want to activate service on <br/> a new iPhone?</h1>
+                <h2>Watch this simple 40-second <br/> overview from TJ.</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>Special stores.</h1>
+              <h1>Exclusive savings for businesses, the military, and more.</h1>
+            </div>
+            <div className='shop14'>
+              <div className='shop24'>
+                <p>BUSINESS</p>
+                <h1>From enterprise to small <br/> business, we'll work with <br/> you.</h1>
+              </div>
+              <div className='shop25'>
+                <p>GOVERNMENT</p>
+                <h1>Special pricing is available <br/> for state, local, and federal <br/> agencies.</h1>
+              </div>
+              <div className='shop26'>
+                <p>VETERANS AND MILITARY</p>
+                <h1>Active and veteran <br/> members may be eligible <br/> for exclusive savings.</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className='shop8'>
+            <div className='shop9'>
+              <h1>The iPhone experience.</h1>
+              <h1>Designed to connect with everything Apple.</h1>
+            </div>
+            <div className='shop14'>
+              <div className='shop27'>
+                <p>APPLE TV+</p>
+                <h1>Get 3 months of Apple TV+ free <br/> when you buy an iPhone.</h1>
+              </div>
+              <div className='shop28'>
+                <p>FITNESS+</p>
+                <h1>Get 3 months free when <br/> you buy an iPhone.</h1>
+              </div>
+              <div className='shop29'>
+                <p>AIRPODS</p>
+                <h1>Magic runs in the family</h1>
+              </div>
+            </div>
+          </div>
     </div>
+
+
+    <div className='footer'>
+            <div className='footer1'>
+                <p className='footer2'>
+                1. Trade-in values will vary based on the condition, year, and configuration of your eligible trade-in device. Not all devices are eligible for credit. You must be at least 18 years old to be eligible to trade in for credit or for an Apple Gift Card. Trade-in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores, and may vary between in-store and online trade-in. Some stores may have additional requirements. Apple or its trade-in partners reserve the right to refuse or limit quantity of any trade-in transaction for any reason. More details are available from Apple’s trade-in partner for trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+                </p>
+                <p>
+Apple Vision Pro has not been authorized as required by the rules of the Federal Communications Commission. This device is not, and may not be, offered for sale or lease, or sold or leased, until authorization is obtained</p>
+                <p>Apple Vision Pro will be available early next year on apple.com and at Apple retail stores in the U.S.</p>
+                <p>To access and use all Apple Card features and products available only to Apple Card users, you must add Apple Card to Wallet on an iPhone or iPad that supports and has the latest version of iOS or iPadOS. Apple Card is subject to credit approval, available only for qualifying applicants in the United States, and issued by Goldman Sachs Bank USA, Salt Lake City Branch.</p>
+                <p>If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about Apple Card.</p>
+            </div>
+
+            <div className='footer3'>
+                <div></div>
+            </div>
+
+            <div className='footer4'>
+                <div className='footer5'>
+                    <div>
+                        <h3>Shop and Learn</h3>
+                        <p>Store</p>
+                        <p>Mac</p>
+                        <p>iPad</p>
+                        <p>iPhone</p>
+                        <p>Watch</p>
+                        <p>Vision</p>
+                        <p>AirPods</p>
+                        <p>TV & Home</p>
+                        <p>Air Tag</p>
+                        <p>Accessories</p>
+                        <p>Gift Cards</p>
+                        <h2>Apple Wallet</h2>
+                        <p>Wallet</p>
+                        <p>Apple Card</p>
+                        <p>Apple Pay</p>
+                        <p>Apple Cash</p>
+                    </div>
+                    <div>
+                        <h3>Account</h3>
+                        <p>Manage Your Apple ID</p>
+                        <p>Apple Store Account</p>
+                        <p>iCloud.com</p>
+                        <h2>Entertainment</h2>
+                        <p>Apple One</p>
+                        <p>Apple TV+</p>
+                        <p>Apple Music</p>
+                        <p>Apple Arcade</p>
+                        <p>Apple Fitness+</p>
+                        <p>Apple News+</p>
+                        <p>Apple Podcasts</p>
+                        <p>Apple Books</p>
+                        <p>App Store</p>
+                    </div>
+                    <div>
+                        <h3>Apple Store</h3>
+                        <p>Find a Store</p>
+                        <p>Genius Bar</p>
+                        <p>Today at Apple</p>
+                        <p>Apple Camp</p>
+                        <p>Apple Store App</p>.
+                        <p>Certified Refurbished</p>
+                        <p>Apple Trade In</p>
+                        <p>Financing</p>
+                        <p>Carrier Deals at Apple</p>
+                        <p>Order Status</p>
+                        <p>Shopping Help</p>
+                    </div>
+                    <div>
+                        <h3>For Business</h3>
+                        <p>Apple and Business</p>
+                        <p>Shop for Business</p>
+                        <h2>For Education</h2>
+                        <p>Apple and Education</p>
+                        <p>Shop for K-12</p>
+                        <p>Shop for College</p>
+                        <h2>For Healthcare</h2>
+                        <p>Apple in Healthcare</p>
+                        <p>Health on Apple Watch</p>
+                        <p>Health Records on iPhone</p>
+                        <h2>For Government</h2>
+                        <p>Shop for Government</p>
+                        <p>Shop for Veterans and Military</p>
+                    </div>
+                    <div>
+                        <h3>Apple Values</h3>
+                        <p>Accessebility</p>
+                        <p>Environment</p>
+                        <p>Inclusion and Diversity</p>
+                        <p>Education</p>
+                        <p>Privacy</p>
+                        <p>Racial Equity and Justice</p>
+                        <p>Supplier Responsibility</p>
+                        <h2>About Apple</h2>
+                        <p>Newsroom</p>
+                        <p>Apple Leadership</p>
+                        <p>Career Opportunities</p>
+                        <p>Investors</p>
+                        <p>Ethics & Compliance</p>
+                        <p>Events</p>
+                        <p>Contact Apple</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</>
   )
 }
 
